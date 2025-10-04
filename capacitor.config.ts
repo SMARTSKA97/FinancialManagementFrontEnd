70a22ab29ca57384f19d6b3fb17d318a7f308bb2
Subhrajit Karmakar <karmakarsubhrajit680@gmail.com>
2025-10-04 18:06:38 +0530

    chore: update package.json to include Capacitor dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ska97.finplanner',
+  appName: 'Financial Planner',
+  webDir: 'dist/angular-app/browser',
+};
+
+export default config;
